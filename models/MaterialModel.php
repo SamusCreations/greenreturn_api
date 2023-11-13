4970a2262a250c446b8f13ebf999940ba6efd0b9
Luijaxx <luija2000@gmail.com>
2023-11-13 15:19:37 -0600

    UploadImage subido

--- models/MaterialModel.php
+++ models/MaterialModel.php
@@ -60,39 +60,92 @@ class MaterialModel
 	}
 
 	public function getColorByMaterialId($materialID)
-{
-    try {
-        // Consulta SQL para obtener el color asociado al material
-        $sql = "SELECT c.* 
+	{
+		try {
+			// Consulta SQL para obtener el color asociado al material
+			$sql = "SELECT c.* 
                 FROM color c
                 INNER JOIN material m ON c.id_color = m.id_color
                 WHERE m.id_material = $materialID";
 
-        // Ejecutar la consulta
-        $result = $this->enlace->ExecuteSQL($sql);
+			// Ejecutar la consulta
+			$result = $this->enlace->ExecuteSQL($sql);
 
-        // Comprobar si se encontró un resultado
-        if ($result) {
-            // Retornar el resultado como un objeto Color
-            return $result;
-        } else {
-            // En caso de no encontrar un resultado, puedes devolver un valor nulo o lanzar una excepción según tus necesidades.
-            return null;
-        }
-    } catch (Exception $e) {
-        die($e->getMessage());
-    }
-}
+			// Comprobar si se encontró un resultado
+			if ($result) {
+				// Retornar el resultado como un objeto Color
+				return $result;
+			} else {
+				// En caso de no encontrar un resultado, puedes devolver un valor nulo o lanzar una excepción según tus necesidades.
+				return null;
+			}
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+	public function uploadImage($file)
+	{
+		$target_dir = __DIR__ . "/photos/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+		// Check if image file is a actual image or fake image
+		if (isset($_POST["submit"])) {
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if ($check !== false) {
+				echo "File is an image - " . $check["mime"] . ".";
+				$uploadOk = 1;
+			} else {
+				echo "File is not an image.";
+				$uploadOk = 0;
+			}
+		}
+
+		// Check if file already exists
+		if (file_exists($target_file)) {
+			echo "Sorry, file already exists.";
+			$uploadOk = 0;
+		}
+
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 500000) {
+			echo "Sorry, your file is too large.";
+			$uploadOk = 0;
+		}
+
+		// Allow certain file formats
+		if (
+			$imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+			&& $imageFileType != "gif"
+		) {
+			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			$uploadOk = 0;
+		}
+
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+			echo "Sorry, your file was not uploaded.";
+			// if everything is ok, try to upload file
+		} else {
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+			} else {
+				echo "Sorry, there was an error uploading your file.";
+			}
+		}
+	}
 
 
 
 
-	public function create($objeto)
+	public function create($objeto, $file)
 	{
 		try {
 			//Consulta sql
 			//Identificador autoincrementable
-
+			$imagePath = $this->uploadImage($file);
 			$vSql = "Insert into material (name, description, image_url, id_measurement, unit_cost, id_color)" .
 				"Values ('$objeto->name', '$objeto->description', '$objeto->image_url', '$objeto->id_measurement', '$objeto->unit_cost', '$objeto->id_color')";
 
